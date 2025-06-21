2b2881047da6893c7b46bb8c6adb049fdad74b45
feuerdev <jannik@feuer.dev>
2025-06-21 14:51:22 +0200

    fix: update transformer lib

--- dist/content.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	'use strict';
-
-	console.log("Content script loaded.");const o=[];chrome.runtime.onMessage.addListener((e,s,n)=>{e.type==="SPONSORED_SEGMENT_FOUND"&&(console.log("Received sponsored segment:",e.payload),o.push(e.payload),console.log("All detected segments:",o));});
-
-})();
