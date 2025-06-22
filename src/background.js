48990ed7d08186258b191af82d444441c68b1eaf
feuerdev <jannik@feuer.dev>
2025-06-22 15:23:25 +0200

    fix: take some stuff from gh

--- src/background.js
+++ src/background.js
@@ -1,4 +1,12 @@
+import { env } from '@xenova/transformers';
 import { classifyText } from './classifier.js';
+
+// Due to a bug in onnxruntime-web, we must disable multithreading for now.
+// See https://github.com/microsoft/onnxruntime/issues/14445 for more information.
+env.backends.onnx.wasm.numThreads = 1;
+env.backends.onnx.wasm.wasmPaths = '/ort/';
+env.allowRemoteModels = true;
+env.allowLocalModels = false;
 
 console.log("Background script loaded.");
 
